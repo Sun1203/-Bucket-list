8856df6b1b69a6fec97d19c478a98e502c284132
Sun1203 <jasun1208@naver.com>
2022-09-01 16:35:51 +0900

    버킷리스트 수정 및 삭제

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ db = client.dbsparta
 
 app = Flask(__name__)
 
-print(hello)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/edit', methods=["GET"])
+def edit():
+    return render_template('edit.html')
 
 
 @app.route('/write', methods=["GET"])
@@ -36,8 +40,11 @@ def write_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     category_receive = request.form['category_give']
+    write_list = list(db.write.find({}, {'_id': False}))
+    count =len(write_list) + 1
 
     doc = {
+        'num':count,
         'url':url_receive,
         'name':name_receive,
         'comment':comment_receive,
@@ -51,6 +58,49 @@ def write_post():
 def write_get():
     write_list = list(db.write.find({}, {'_id': False}))
     return jsonify({'write': write_list})
+
+
+@app.route("/edit_show", methods=["POST"])
+def edit_show():
+    edit_list = list(db.write.find({}, {'_id': False}))
+    return jsonify({'edit_list': edit_list})
+
+
+@app.route("/edit_get", methods=["POST", "GET"])
+def edit_get():
+    url_receive = request.form['url_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+
+    data = {
+        'url': url_receive,
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    if request.method == "POST":
+        return jsonify({'data': data})
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    name_receive = request.form['name_give']
+    print(name_receive)
+    db.write.delete_one({'name': name_receive})
+    return jsonify({'msg': "삭제 완료!"})
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    url_receive = request.form['url_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    category_receive = request.form['category_give']
+
+    db.write.update_one({'name': name_receive}, {'$set': {'comment': comment_receive}})
+    db.write.update_one({'name': name_receive}, {'$set': {'url': url_receive}})
+    db.write.update_one({'name': name_receive}, {'$set': {'category': category_receive}})
+    return jsonify({'update': "수정 완료!"})
 
 
 if __name__ == '__main__':
